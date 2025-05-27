450afff014a09209707d1724580f68fa44b0a620
Ejay <ejaymarqueses3@gmail.com>
2025-05-27 18:52:12 +0800

    Refactor: split pages, add monochrome theme

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Typing Effect
+const phrases = ["a BSIT Student", "a Web Developer", "learning everyday"];
+const typingText = document.querySelector(".typing-text");
+let index = 0, charIndex = 0;
+
+function type() {
+  if (charIndex < phrases[index].length) {
+    typingText.textContent += phrases[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = phrases[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    index = (index + 1) % phrases.length;
+    setTimeout(type, 300);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (typingText) type();
+});
